314830817774384d9619edcd9873cb405b8e8289
agustinalcoba <agustinalcoba707978@gmail.com>
2024-09-12 15:44:13 -0300

    .env added to db file

--- modules/db.js
+++ modules/db.js
@@ -1,9 +1,13 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "upload-images-react",
+  host: process.env.MYSQL_ADDON_HOST,
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  database: process.env.MYSQL_ADDON_DB,
+  // host: "localhost",
+  // user: "root",
+  // password: "",
+  // database: "upload-images-react",
 });
 module.exports = connection;
